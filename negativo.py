7b06f4f60c39384b61966e9783f46bce66f03451
lerneri <leticiarneri@alu.ufc.br>
2023-10-15 22:40:35 -0300

    Foi?

--- negativo.py
+++ negativo.py
@@ -24,21 +24,3 @@ def negativo_RGB(img,salvar):
             nova_imagem.point((coluna,linha),fill=negativo(r,g,b))
 
     imagem_cinza.save(salvar)
-
-def negativo_simples(path,salvar):
-    img = Image.open(path)
-    data = asarray(img) / 255.0
-    data = (1 - data)*255
-    img2 = Image.fromarray(data)
-    img.close()
-    img2.save(salvar)
-    img2.close()
-
-def negativo_RGB(path, salvar):
-    img = Image.open(path)
-    data = np.array(img)
-    inverted_data = 255 - data
-    img2 = Image.fromarray(inverted_data)
-    img.close()
-    img2.save(salvar)
-    img2.close()
